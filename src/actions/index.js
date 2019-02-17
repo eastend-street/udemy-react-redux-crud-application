7356fe402bf423eb6c795ea43d2c81fc671fa341
eastend-street <ss_1322irf@icloud.com>
2019-02-16 16:48:23 -0800

    implement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// action createrを定義
+
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+
+export const increment = () => ({
+  type: INCREMENT
+});
+
+export const decrement = () => ({
+  type: DECREMENT
+});
