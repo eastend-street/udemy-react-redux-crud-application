39f1e981874c30741e58292526f50c6c16ed2f30
eastend-street <suminari.yamada@gmail.com>
2019-02-20 17:50:52 -0800

    implement delete feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { Field, reduxForm } from "redux-form";
 
-import { postEvent } from "../actions";
+import { getEvent, deleteEvent, putEvent } from "../actions";
 
-class EventsNew extends Component {
+class EventsShow extends Component {
   constructor(props) {
     super(props);
     this.onSubmit = this.onSubmit.bind(this);
+    this.onDeleteClick = this.onDeleteClick.bind(this);
   }
 
   renderField(field) {
@@ -27,8 +28,14 @@ class EventsNew extends Component {
     );
   }
 
+  async onDeleteClick() {
+    const { id } = this.props.match.params;
+    await this.props.deleteEvent(id)
+    this.props.history.push("/");
+  }
+
   async onSubmit(values) {
-    await this.props.postEvent(values);
+    // await this.props.postEvent(values);
     this.props.history.push("/");
   }
 
@@ -59,6 +66,9 @@ class EventsNew extends Component {
             disabled={pristine || submitting}
           />
           <Link to="/">Cancel</Link>
+          <Link to="/" onClick={this.onDeleteClick}>
+            Delete
+          </Link>
         </div>
       </form>
     );
@@ -73,9 +83,9 @@ const validate = values => {
 
   return errors;
 };
-const mapDispatchToProps = { postEvent };
+const mapDispatchToProps = { deleteEvent };
 
 export default connect(
   null,
   mapDispatchToProps
-)(reduxForm({ validate, form: "eventNewFrom" })(EventsNew));
+)(reduxForm({ validate, form: "eventShowFrom" })(EventsShow));
